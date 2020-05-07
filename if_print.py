81bd1bded405449f99b39b8dd3ef4bb9b32b6a7f
codebuilder <2pruthvi@gmail.com>
2020-05-07 07:25:16 +0000

    if_print.py

---
+++ if_print.py
@@ -0,0 +1,10 @@
+# if & print syntax
+# Filename: if_print.py
+
+i = 0
+if i < 0: 
+    print("i=", i, "Negative Number")
+elif i > 0:
+    print("i=", i, "Positive Number")
+else:
+    print("i=", i, "Zero")
