c1b28601fc65a91efeeb6aa8269398085bfa1813
codebuilder <2pruthvi@gmail.com>
2020-05-07 07:32:13 +0000

    updated if_print.py

--- if_print.py
+++ if_print.py
@@ -1,10 +1,10 @@
 # if & print syntax
 # Filename: if_print.py
 
-i = 0
+i = 22
 if i < 0: 
-    print("i=", i, "=> Negative Number")
+    print("i=", i, "=> Negative number")
 elif i > 0:
-    print("i=", i, "=> Positive Number")
+    print("i=", i, "=> Positive number")
 else:
     print("i=", i, "=> Zero")
